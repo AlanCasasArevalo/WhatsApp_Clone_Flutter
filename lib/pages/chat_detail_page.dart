76bcbbdf516d99405a38430e8060facde50ce0d9
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-12 16:46:06 +0100

    [FEATURE][STATUS] * Chat detail was improved with textfield

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/models/chat_model.dart';
 
 class ChatDetailPage extends StatefulWidget {
-
   final String name;
+
   ChatDetailPage({this.name});
 
   @override
@@ -11,12 +10,52 @@ class ChatDetailPage extends StatefulWidget {
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
+  TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
       ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: _textProvider(),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget _textProvider() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+          child: Row(
+        children: [
+          Flexible(
+              child: TextField(
+            textAlignVertical: TextAlignVertical.bottom,
+            textAlign: TextAlign.start,
+            controller: _textEditingController,
+          )),
+          IconButton(icon: Icon(Icons.send), onPressed: () => _handledSubmit(_textEditingController.text))
+        ],
+      )),
+    );
+  }
+
+  void _handledSubmit(String text) {
+
   }
 }
